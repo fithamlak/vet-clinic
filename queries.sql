27576f779aef8dfbca106910a0476ad71759172d
Fithamlak <justiceofgod01@gmail.com>
2023-04-04 01:32:37 +0300

    List the name of all animals that are neutered and have less than 3 escape attempts.

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 select * from animals where name like '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name FROM animals WHERE neutered = 'true' and escape_attepts < 3;
+SELECT name FROM animals WHERE neutered = TRUE and escape_attepts < 3;
 SELECT date_of_birth from animals WHERE name IN ('Agumen', 'Pikachu'); 
 SELECT name, escape_attepts from animals WHERE weight_kg > 10.5;
 SELECT *  from animals WHERE name != 'Gabumon';
