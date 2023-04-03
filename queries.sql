b19878d8dc658d1a86ea19959d48e9fd86553598
Fithamlak <justiceofgod01@gmail.com>
2023-04-04 02:35:11 +0300

    Add Find all animals that are neutered in queries.sql file

--- queries.sql
+++ queries.sql
@@ -6,6 +6,7 @@ SELECT name FROM animals WHERE neutered = TRUE and escape_attepts < 3;
 SELECT date_of_birth from animals WHERE name IN ('Agumen', 'Pikachu'); 
 SELECT name, escape_attepts from animals WHERE weight_kg > 10.5;
 SELECT *  from animals WHERE name != 'Gabumon';
+SELECT * from animals WHERE neutered = TRUE;
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 and 17.3;
 UPDATE animals
 SET name = 'Agumon'
