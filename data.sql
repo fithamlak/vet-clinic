70a8cec39b4bfd3d380245ce55298b0494db280c
Fithamlak <justiceofgod01@gmail.com>
2023-04-04 00:20:18 +0300

    Insert Gabumon into animal table

--- data.sql
+++ data.sql
@@ -2,3 +2,6 @@
 
 INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
 VALUES ('Agumen', 'Feb 03, 2020', 0, true, 10.23 );
+
+INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
+VALUES ('Gabumon', 'Nov 15, 2018', 2, true, 8 );
