582c6cc4c1654c543fb4232d52021c1f2e09a038
Fithamlak <justiceofgod01@gmail.com>
2023-04-04 00:25:26 +0300

    Insert Devimon into animal table

--- data.sql
+++ data.sql
@@ -8,3 +8,6 @@ VALUES ('Gabumon', 'Nov 15, 2018', 2, true, 8 );
 
 INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
 VALUES ('Pikachu', 'Jan 07, 2021', 1, false, 15.04 );
+
+INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
+VALUES ('Devimon', 'May 12, 2017', 5, true, 11 );
