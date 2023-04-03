6d90348703533c243fa488714656d7c66e7a483b
Fithamlak <justiceofgod01@gmail.com>
2023-04-04 01:02:18 +0300

    List the name of all animals born between 2016 and 2019.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 select * from animals where name like '%mon';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
