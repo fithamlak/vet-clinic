b162d0f5a064605503373dbf741c3a232fc3a246
Fithamlak <justiceofgod01@gmail.com>
2023-04-04 01:29:53 +0300

    Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)

--- queries.sql
+++ queries.sql
@@ -6,3 +6,4 @@ SELECT name FROM animals WHERE neutered = 'true' and escape_attepts < 3;
 SELECT date_of_birth from animals WHERE name IN ('Agumen', 'Pikachu'); 
 SELECT name, escape_attepts from animals WHERE weight_kg > 10.5;
 SELECT *  from animals WHERE name != 'Gabumon';
+SELECT * from animals WHERE weight_kg BETWEEN 10.4 and 17.3;
