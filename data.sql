1a72fde167387a8cc8643047b9f10c64c889eb4d
Fithamlak <justiceofgod01@gmail.com>
2023-04-04 00:17:22 +0300

    Insert Agumen into animal table

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
+VALUES ('Agumen', 'Feb 03, 2020', 0, true, 10.23 );
