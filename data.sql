0b5b4bf4b30799aa764a562e95769ba3a7051231
Fithamlak <justiceofgod01@gmail.com>
2023-04-04 00:23:26 +0300

    Insert Pikachu into animal table

--- data.sql
+++ data.sql
@@ -5,3 +5,6 @@ VALUES ('Agumen', 'Feb 03, 2020', 0, true, 10.23 );
 
 INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
 VALUES ('Gabumon', 'Nov 15, 2018', 2, true, 8 );
+
+INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
+VALUES ('Pikachu', 'Jan 07, 2021', 1, false, 15.04 );
