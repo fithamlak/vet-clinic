b0d1ae5aa780127729c29e1632a02674266277b9
Fithamlak <justiceofgod01@gmail.com>
2023-04-03 23:57:31 +0300

    Copy the SQL  used to create a table named animals with the given columns

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+	id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(40),
+    date_of_birth date,
+	escape_attepts int,
+	neutered boolean,
+	weight_kg decimal
 );
