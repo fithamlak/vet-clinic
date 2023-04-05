ea5e90112f7f69ff2f951bcc6e17fd386520b39f
Fithamlak <justiceofgod01@gmail.com>
2023-04-05 10:32:01 +0300

    Insert one animal whose name is Charmander into animal table

--- data.sql
+++ data.sql
@@ -11,3 +11,6 @@ VALUES ('Pikachu', 'Jan 07, 2021', 1, false, 15.04 );
 
 INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
 VALUES ('Devimon', 'May 12, 2017', 5, true, 11 );
+
+INSERT INTO animals (name, date_of_birth, escape_attepts, neutered, weight_kg)
+VALUES ('Charmander', 'Feb 08, 2020', 0, false, 11 );
