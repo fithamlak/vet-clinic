faa724765fbb2c6ca7a757f26c3b1791d5ec8d0a
Fithamlak <justiceofgod01@gmail.com>
2023-04-08 20:07:06 +0300

    Give clear comments for each queries in queris.sql file

--- queries.sql
+++ queries.sql
@@ -8,37 +8,52 @@ SELECT name, escape_attepts from animals WHERE weight_kg > 10.5;
 SELECT *  from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE neutered = TRUE;
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 and 17.3;
+
+-- update name of animal from Agumen to Agumon
 UPDATE animals
 SET name = 'Agumon'
 WHERE name = 'Agumen';
 
+--Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. 
+--Then roll back the change and verify that the species columns went back to the state before the transaction.
 BEGIN TRANSACTION;
-
 UPDATE animals
 SET species = 'unspecified';
 SELECT * from animals;
 ROLLBACK;
 SELECT * from animals;
 
+--Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 BEGIN TRANSACTION;
-
 UPDATE animals
 SET species = 'digimon'
 WHERE name LIKE '%mon';
 
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+-- Commit the transaction.
 UPDATE animals
 SET species = 'pokemon'
 WHERE species IS NULL OR species = '';
 SELECT * FROM animals;
 COMMIT;
+-- Verify that change was made and persists after commit.
 SELECT * FROM animals;
 
+-- Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
 BEGIN;
 DELETE FROM animals;
 SELECT COUNT(*) FROM ANIMALS;
 ROLLBACK;
+-- After the rollback verify if all records in the animals table still exists. After that, you can start breathing as usual ;)
 SELECT COUNT(*) FROM ANIMALS;
 
+--nside a transaction:
+  --Delete all animals born after Jan 1st, 2022.
+  --Create a savepoint for the transaction.
+  --Update all animals' weight to be their weight multiplied by -1.
+  --Rollback to the savepoint
+  --Update all animals' weights that are negative to be their weight multiplied by -1.
+  --Commit transaction
 BEGIN;
 DELETE FROM animals
 WHERE date_of_birth > '2022-01-01';
@@ -51,59 +66,71 @@ SET weight_kg = -1 * weight_kg
 WHERE weight_kg < 0;
 COMMIT;
 
+-- How many animals are there?
 SELECT COUNT(*) FROM animals;
+-- How many animals have never tried to escape?
 SELECT COUNT(*) FROM animals WHERE escape_attepts = 0;
+-- What is the average weight of animals?
 SELECT AVG(weight_kg) FROM animals;
 
+-- Who escapes the most, neutered or not neutered animals?
 SELECT neutered, SUM(escape_attepts) AS total_escapes
 FROM animals
 GROUP BY neutered
 ORDER BY total_escapes DESC;
 
+-- What is the minimum and maximum weight of each type of animal?
 SELECT neutered, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
 FROM animals
 GROUP BY neutered;
 
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT neutered, AVG(escape_attepts) AS avg_escape_attempts
 FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY neutered;
 
+-- What animals belong to Melody Pond?
 SELECT animals .*, full_name
 FROM animals
 JOIN owners
 ON animals.owner_id = owners.id
 WHERE owners.full_name = 'Melody Pond';
 
+-- List of all animals that are pokemon (their type is Pokemon).
 SELECT , species.name
 FROM animals
 JOIN species
 ON animals.species_id = species.id
 WHERE species.name = 'Pokemon';
 
+-- List all owners and their animals, remember to include those that don't own any animal.
 SELECT owners.id, full_name, age, name
 FROM owners
 LEFT JOIN animals 
    ON owners.id = animals.owner_id;
 
+-- How many animals are there per species?
 SELECT species_id, COUNT(*) AS total_numbers
 FROM animals
 GROUP BY species_id
 ORDER BY total_numbers DESC;   
 
+-- List all Digimon owned by Jennifer Orwell.
 SELECT animals .*, full_name
 FROM animals
 JOIN owners
 ON animals.owner_id = owners.id
 WHERE owners.full_name = 'Jennifer Orwell' and species_id = 2 ;
 
-
+-- List all animals owned by Dean Winchester that haven't tried to escape.
 SELECT animals .*, full_name
 FROM animals
 JOIN owners
 ON animals.owner_id = owners.id
 WHERE owners.full_name = 'Dean Winchester' and escape_attepts = 0;
 
+-- Who owns the most animals?
 SELECT owner_id, count(*) AS total_numer_of_animals
 FROM animals
 GROUP BY owner_id
